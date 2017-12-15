a093d40aa9155c4aac9a4c1c5f1708b3e8947272
Victor Nunes <victornunes@lett.com.br>
2017-12-15 19:44:36 -0200

    implementing

--- main.js
+++ main.js
@@ -1,23 +1,31 @@
-var linksPopup;
-var mainPopup;
-var myUrlsArray = [
-    "http://vitim.us",
-    "http://oldblog.vitim.us",
-    "http://vitim.com.br",
-    "http://www.instructables.com/member/Vitim/",
-].map((url) => { return {url, meta: 0};});
-
 var listView;
 
+var linksPopup;
 var mainPopup;
+
+
+var param = new ParamManager();
+
+
+var data = {
+    index: 0,
+    name: '',
+    note: '',
+    urls: [],
+};
+
+
+
+
 function goTo(url) {
     if (mainPopup === undefined || mainPopup.window.closed) {
-        openPopup();
+        openSitePopup();
     }
     mainPopup.window.location = url;
 }
 
-function openPopup() {
+function openSitePopup() {
+
     mainPopup = new Popup('wait.html', 'site', {
         minimizable: false,
         status: false,
@@ -28,12 +36,12 @@ function openPopup() {
         alwaysRaised: true,
         scrollbars: true,
         resizable: true,
-        left: ((window.screen.availWidth || window.screen.width) - (linksPopup.window.screenLeft + linksPopup.window.outerWidth)),
+        left: ((linksPopup.window.screenLeft) + linksPopup.window.outerWidth),
         top: 0,
         height: (window.screen.availHeight || window.screen.height),
-        width: ((window.screen.availWidth || window.screen.width) - (linksPopup.window.screenLeft + linksPopup.window.outerWidth)),
+        width: ((linksPopup.window.screen.availWidth || linksPopup.window.screen.width) - ((linksPopup.window.screenLeft - linksPopup.window.screen.availLeft) + linksPopup.window.outerWidth)),
     });
-    
+
 
     if (mainPopup.window) {
         console.log(mainPopup);
@@ -44,64 +52,13 @@ function openPopup() {
 
 window.onload = function () {
 
-
-    listView = new Vue({
-        el: '#viewer',
-        data: function () {
-            return {
-                name: 'asd',
-                notes: 'asdjaoij',
-                items: myUrlsArray
-            }
-        },
-        methods: {
-            remove(i) {
-                this.items.splice(i, 1);
-            }
-        }
-    });
-
-    selectView = new Vue({
-        el: '#popupcontent',
-        data: function () {
-            return {
-                current: 0,
-                items: myUrlsArray
-            }
-        },
-        methods: {
-            next(e) {
-                console.log(e);
-                if (this.current < this.items.length-1) this.current = this.current + 1;
-            },
-            prev(e) {
-                console.log(e);
-                if (this.current > 0) this.current = this.current - 1;
-            },
-            selectClick(e) {
-                var i = parseInt(e.currentTarget.value);
-                console.log(this.items[i]);
-
-                goTo(this.items[i].url);
-            },
-            selectChange(e) {
-                //this.items.splice(i, 1);
-                var i = parseInt(e.currentTarget.value);
-                console.log(this.items[i]);
-                this.current = i;
-            },
-            flag(x) { 
-                this.items[this.current].meta = x;
-            }
-        }
-    });
-    
     //close popups if closing main window
     window.addEventListener('beforeunload', function (e) {
         console.log('closing main window', e);
         if (linksPopup) linksPopup.close();
         if (mainPopup) mainPopup.close();
     });
+
 
 
 
@@ -114,25 +71,25 @@ window.onload = function () {
             }
         });
 
-        urls = urls.map(function (url, i) {
+        data.urls = urls.map(function (url, i) {
             return {
                 url: url,
                 meta: 0
             };
         });
 
-        myUrlsArray = urls;
-        listView.items = myUrlsArray;
-        selectView.items = myUrlsArray;
+        // myUrlsArray = urls;
+        // listView.items = myUrlsArray;
+        // selectView.items = myUrlsArray;
 
         //TODO show loaded urls in a list
     };
 
-    
-    
-    
+
+
+
     start.onclick = function () {
-    
+
         linksPopup = new Popup('', 'audit', {
             minimizable: false,
             status: false,
@@ -143,108 +100,119 @@ window.onload = function () {
             alwaysRaised: true,
             scrollbars: true,
             resizable: true,
-            left: false,
-            top: false,
+            left: 0,
+            top: 0,
             height: (window.screen.availHeight || window.screen.height),
             width: (window.screen.availWidth || window.screen.width) / 3,
         });
-    
+
         if (linksPopup.window) {
             console.log(linksPopup);
-    
+
             allowPopups.style.display = "none";
-    
+
             start.disabled = true;
-            
+
             linksPopup.onclose = function () {
                 start.disabled = false;
             };
-    
+
             renderPopup(linksPopup.window);
         } else {
             console.error('Popup blocked by browser!');
             allowPopups.style.display = "";
         }
-    
+
     };
-    
-    function renderPopup(p) { 
+
+    function renderPopup(p) {
         p.document.body.appendChild(popupcontent.firstElementChild);
     }
 
 
-    linksPopup = new Popup('', 'audit', {
-            minimizable: false,
-            status: false,
-            personalbar: false,
-            location: false,
-            toolbar: false,
-            menubar: false,
-            alwaysRaised: true,
-            scrollbars: true,
-            resizable: true,
-            left: ((window.screen.availWidth || window.screen.width) / 3) ,
-            top: false,
-            height: (window.screen.availHeight || window.screen.height),
-            width: ((window.screen.availWidth || window.screen.width) / 3)*2,
-        });
-    
-        if (linksPopup.window) {
-            console.log(linksPopup);
-    
-            allowPopups.style.display = "none";
-    
-            start.disabled = true;
-            
-            linksPopup.onclose = function () {
-                start.disabled = false;
-            };
-    
-            renderPopup(linksPopup.window);
-        } else {
-            console.error('Popup blocked by browser!');
-            allowPopups.style.display = "";
+    listView = new Vue({
+        el: '#viewer',
+        data: function () {
+            return data;
+        },
+        methods: {
+            remove(i) {
+                this.urls.splice(i, 1);
+            }
         }
-    
+    });
+
+    selectView = new Vue({
+        el: '#popupcontent',
+        data: function () {
+            return data;
+        },
+        methods: {
+            next(e) {
+                console.log(e);
+                if (this.index < this.urls.length - 1) this.index = this.index + 1;
+            },
+            prev(e) {
+                console.log(e);
+                if (this.index > 0) this.index = this.index - 1;
+            },
+            selectClick(e) {
+                var i = parseInt(e.target.value);
+                console.log(this.urls[i]);
+
+                goTo(this.urls[i].url);
+            },
+            selectChange(e) {
+                //this.urls.splice(i, 1);
+                var i = parseInt(e.target.value);
+                console.log(this.urls[i]);
+                this.index = i;
+            },
+            flag(x) {
+                this.urls[this.index].meta = x;
+            }
+        }
+    });
+
+
+
+
+    function updateParams() {
+        param.setAll({
+            i: data.index,
+            name: data.name,
+            note: data.note,
+            meta: data.urls.map((obj,i) => data.urls[i].meta || 0),
+            urls: data.urls.map((obj) => obj.url),
+        });
+    }
+
+    function readParams() {
         
-    // urlsSelect.onclick = function () {
-        
-    //         mainPopup = new Popup('', 'main', {
-    //             minimizable: false,
-    //             status: false,
-    //             personalbar: false,
-    //             location: false,
-    //             toolbar: false,
-    //             menubar: false,
-    //             alwaysRaised: true,
-    //             scrollbars: true,
-    //             resizable: true,
-    //             left: (window.screen.availWidth || window.screen.width) / 3,
-    //             top: false,
-    //             height: (window.screen.availHeight || window.screen.height),
-    //             width: ((window.screen.availWidth || window.screen.width) / 3)*2,
-    //         });
-
-    //         if (mainPopup.window) {
-    //             console.log(mainPopup);
-
-    //             allowPopups.style.display = "none";
-
-    //             start.disabled = true;
-
-    //             mainPopup.onclose = function () {
-    //                 start.disabled = false;
-    //             };
-
-    //             renderPopup(mainPopup.window);
-    //         } else {
-    //             console.error('Popup blocked by browser!');
-    //             allowPopups.style.display = "";
-    //         }  
-    //     };
+        var p = param.getAll();
+        data.index = p.i || 0;
+        data.name = p.name;
+        data.note = p.note;
     
-    //     urlsSelect.onchange = urlsSelect.onclick;
-    
+        if (p.urls) {
+            data.urls = p.urls.map((url, i) => {
+                return {
+                    url: url,
+                    meta: p.meta[i]
+                };
+            });
+        }
+
+
+    }
+
+    readParams();
+
+    listView.$watch('index', updateParams);
+    listView.$watch('name', updateParams);
+    listView.$watch('note', updateParams);
+    listView.$watch('urls', updateParams);
+    listView.$watch('urls.meta', updateParams);
 
 
 
